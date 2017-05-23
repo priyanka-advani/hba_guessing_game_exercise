ddbf5468a493a827fb05ca6096b417513404f6cd
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-23 15:55:08 -0700

    made sure user guess is an int

--- game.py
+++ game.py
@@ -13,16 +13,20 @@ random_number = randint(1, 100)
 number_of_guesses = 0
 
 while True:
-    player_guess = int(raw_input('What is your guess? '))
-    number_of_guesses += 1
-    if player_guess < 1 or player_guess > 100:
-        print 'Please enter a number between 1 and 100 '
-    else:
-        if player_guess != random_number:
-            if player_guess > random_number:
-                print 'Your guess is too high. Try again.'
-            elif player_guess < random_number:
-                print 'Your guess is too low. Try again.'
+    player_guess = raw_input('What is your guess? ')
+    if player_guess.isdigit():
+        player_guess = int(player_guess)
+        if player_guess < 1 or player_guess > 100:
+            print 'Please enter a number between 1 and 100 '
         else:
-            print 'Congratulations %s, you guessed the correct number in %d guesses!' % (player_name, number_of_guesses)
-            break
+            number_of_guesses += 1
+            if player_guess != random_number:
+                if player_guess > random_number:
+                    print 'Your guess is too high. Try again.'
+                elif player_guess < random_number:
+                    print 'Your guess is too low. Try again.'
+            else:
+                print 'Congratulations %s, you guessed the correct number in %d guesses!' % (player_name, number_of_guesses)
+                break
+    else:
+        print 'Please enter a valid number.'
